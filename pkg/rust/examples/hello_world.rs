3890ac28ad1540ec0318ca7d97ff068c3cc0b73c
4roring <4roring@naver.com>
2023-06-16 00:31:40 +0900

    Change to use AST in hello_world example

--- pkg/rust/examples/hello_world.rs
+++ pkg/rust/examples/hello_world.rs
@@ -5,6 +5,7 @@ mod hello_world {
             prelude::{Glue, Payload, Value},
             sled_storage::SledStorage,
         },
+        gluesql_core::ast_builder::{table, text, Execute},
         std::fs,
     };
 
@@ -25,30 +26,39 @@ mod hello_world {
 
         /*
             Create table then insert a row
-
-            Write queries as a string
         */
-        let queries = "
-            CREATE TABLE greet (name TEXT);
-            INSERT INTO greet VALUES ('World');
-        ";
+        table("greet")
+            .create_table()
+            .add_column("name TEXT")
+            .execute(&mut glue)
+            .await
+            .expect("Failed Create Table");
 
-        glue.execute(queries).await.expect("Execution failed");
+        /*
+            Insert row
+        */
+        table("greet")
+            .insert()
+            .columns("name")
+            .values(vec![vec![text("World")]])
+            .execute(&mut glue)
+            .await
+            .expect("Failed Insert Data");
 
         /*
             Select inserted row
         */
-        let queries = "
-            SELECT name FROM greet
-        ";
-
-        let result = glue.execute(queries).await.expect("Failed to execute");
+        let result = table("greet")
+            .select()
+            .project("name")
+            .execute(&mut glue)
+            .await
+            .expect("Failed Select Data");
 
         /*
             Query results are wrapped into a payload enum, on the basis of the query type
         */
-        assert_eq!(result.len(), 1);
-        let rows = match &result[0] {
+        let rows = match &result {
             Payload::Select { labels: _, rows } => rows,
             _ => panic!("Unexpected result: {:?}", result),
         };
